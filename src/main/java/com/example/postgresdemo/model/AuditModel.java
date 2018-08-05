d499f2eb3080e04224258e768ebc9c58b1d8b03d
kamaumike <kamaumike2013@gmail.com>
2018-08-05 16:21:56 +0300

    Add getters and setters

--- src/main/java/com/example/postgresdemo/model/AuditModel.java
+++ src/main/java/com/example/postgresdemo/model/AuditModel.java
@@ -30,4 +30,21 @@ public abstract class AuditModel implements Serializable {
 	@LastModifiedDate
 	private Date updatedAt;
 
+	// Getters and setters
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}	
+
 }
